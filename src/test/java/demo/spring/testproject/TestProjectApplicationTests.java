9aa6d2ed4f94bc0cf6e27e0a93d392533a622644
intheinterviewprep <intheinterviewprep@gmail.com>
2023-01-03 20:19:04 -0600

    initial draft

---
+++ src/test/java/demo/spring/testproject/TestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.spring.testproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
